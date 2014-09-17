f6b53d325a5258b104b7a6ae7f4902385c337cd8
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-17 20:13:32 +0000

    File implementation

---
+++ libgpmat/src/gp-file.c
@@ -0,0 +1,108 @@
+/* Remote access
+--  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+*/
+#include "config.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gp-remote.h"
+#include "gp-file.h"
+
+struct gp_file_server 
+{
+  struct gp_server root;
+  int fd;
+  unsigned char* write_ptr;
+  unsigned char* last_ptr;
+  unsigned char buffer[4096];
+};
+
+static struct gp_file_server server;
+
+static int gp_file_flush (struct gp_file_server* file)
+{
+  size_t sz = file->write_ptr - file->buffer;
+  ssize_t res = write (file->fd, file->buffer, sz);
+  if (res != sz)
+    {
+      close (file->fd);
+      file->fd = -1;
+      return -1;
+    }
+  file->write_ptr = file->buffer;
+  return 0;
+}
+
+void gp_file_send (struct gp_server* server, const void* ptr, size_t len)
+{
+   struct gp_file_server* file = (struct gp_file_server*) server;
+
+   if (file->fd < 0)
+     return;
+
+   while (len > 0)
+     {
+       size_t avail = file->last_ptr - file->write_ptr;
+       if (avail > len)
+         avail = len;
+
+       if (avail > 0)
+         {
+           memcpy (file->write_ptr, ptr, avail);
+           file->write_ptr += avail;
+           ptr = ((unsigned char*) ptr) + avail;
+           len -= avail;
+         }
+       else
+         {
+           if (gp_file_flush (file) != 0)
+             break;
+         }
+     }
+}
+
+void gp_file_synchronize (struct gp_server* server)
+{
+}
+
+void gp_file_close (struct gp_server* server)
+{
+   struct gp_file_server* file = (struct gp_file_server*) server;
+
+   if (file->fd < 0)
+     return;
+
+   if (gp_file_flush (file) == 0)
+     close (file->fd);
+   file->fd = -1;
+}
+
+struct gp_file_server* gp_file_open (const char* param)
+{
+  server.fd = open (param, O_CREAT | O_WRONLY | O_TRUNC | O_CLOEXEC, 0644);
+   if (server.fd <= 0) 
+     {
+	return NULL;
+     }
+   server.write_ptr = server.buffer;
+   server.last_ptr  = &server.buffer[sizeof (server.buffer)];
+   server.root.to_send        = gp_file_send;
+   server.root.to_synchronize = gp_file_synchronize;
+   server.root.to_close       = gp_file_close;
+   
+   return &server;
+}
