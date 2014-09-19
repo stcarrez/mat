fb2376ad5b8885531873ba2e08aa936cda2ae5f7
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-19 20:10:46 +0000

    Target memory representation

---
+++ mat/src/memory/mat-memory-targets.adb
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------
+--  Memory Events - Definition and Analysis of memory events
+--  Copyright (C) 2014 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with MAT.Types; use MAT.Types;
+with MAT.Memory.Events.Servant;
+package body MAT.Memory.Clients is
+
+   use MAT.Memory.Events.Servant;
+
+   procedure Create_Instance (Refs : in ClientInfo_Ref_Map) is
+      --  Adapter : Manager       := Get_Manager (Refs);
+      --  Client  : Client_Memory := new Client_Memory;
+   begin
+      --  Register_Client (Refs, "memory", Client.all'Access);
+      --  Register_Servant (Adapter, Proxy);
+      null;
+   end Create_Instance;
+
+end MAT.Memory.Clients;
