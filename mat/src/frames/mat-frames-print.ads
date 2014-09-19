9bbff0cf991c77af622502c48846a642f8814086
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-19 19:43:02 +0000

    Dump the stack frame

---
+++ mat/src/frames/mat-frames-print.ads
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------
+--  Frames - Representation of stack frames
+--  Copyright (C) 2014 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO; use Ada.Text_IO;
+procedure MAT.Frames.Print (File : in File_Type;
+                                  F : in Frame_Ptr);
