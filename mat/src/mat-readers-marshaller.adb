fc4dc96dc53b32c1ded884d6a2c91e83fc4f1e8f
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-19 20:09:14 +0000

    Operations to read information from a message

---
+++ mat/src/mat-readers-marshaller.adb
@@ -0,0 +1,90 @@
+-----------------------------------------------------------------------
+--  Ipc -- Ipc channel between profiler tool and application          --
+--  Copyright (C) 2014 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with System; use System;
+with System.Address_To_Access_Conversions;
+with System.Storage_Elements;
+with MAT.Types;
+with Interfaces; use Interfaces;
+package body MAT.Readers.Marshaller is
+
+   use System.Storage_Elements;
+
+   package Uint8_Access is new System.Address_To_Access_Conversions (MAT.Types.Uint8);
+
+   package Uint32_Access is new System.Address_To_Access_Conversions (MAT.Types.Uint32);
+
+   function Get_Raw_Uint32 (Buf : System.Address) return MAT.Types.Uint32 is
+      use Uint32_Access;
+
+      P : Object_Pointer := To_Pointer (Buf);
+   begin
+      return P.all;
+   end Get_Raw_Uint32;
+
+   function Get_Uint8 (Buffer : in Buffer_Ptr) return MAT.Types.Uint8 is
+      use Uint8_Access;
+
+      P : Object_Pointer := To_Pointer (Buffer.Current);
+   begin
+      if Buffer.Size = 0 then
+         raise Buffer_Underflow_Error;
+      end if;
+      Buffer.Size := Buffer.Size - 1;
+      Buffer.Current := Buffer.Current + Storage_Offset (1);
+      return P.all;
+   end Get_Uint8;
+
+   function Get_Uint16 (Buffer : in Buffer_Ptr) return MAT.Types.Uint16 is
+      use Uint8_Access;
+
+      High : Object_Pointer := To_Pointer (Buffer.Current
+                                           + Storage_Offset (1));
+      Low : Object_Pointer := To_Pointer (Buffer.Current);
+   begin
+      if Buffer.Size <= 1 then
+         raise Buffer_Underflow_Error;
+      end if;
+      Buffer.Size := Buffer.Size - 2;
+      Buffer.Current := Buffer.Current + Storage_Offset (2);
+      return MAT.Types.Uint16 (High.all) * 256 + MAT.Types.Uint16 (Low.all);
+   end Get_Uint16;
+
+   function Get_Uint32 (Buffer : in Buffer_Ptr) return MAT.Types.Uint32 is
+      use Uint32_Access;
+
+      P : Object_Pointer := To_Pointer (Buffer.Current);
+   begin
+      if Buffer.Size < 4 then
+         raise Buffer_Underflow_Error;
+      end if;
+      Buffer.Size := Buffer.Size - 4;
+      Buffer.Current := Buffer.Current + Storage_Offset (4);
+      if Buffer.Current >= Buffer.Last then
+         Buffer.Current := Buffer.Start;
+      end if;
+      return P.all;
+   end Get_Uint32;
+
+   function Get_Uint64 (Buffer : in Buffer_Ptr) return MAT.Types.Uint64 is
+      Val : MAT.Types.Uint64 := MAT.Types.Uint64 (Get_Uint32 (Buffer));
+   begin
+      return Val + MAT.Types.Uint64 (Get_Uint32 (Buffer)) * 2**32;
+   end Get_Uint64;
+
+end MAT.Readers.Marshaller;
