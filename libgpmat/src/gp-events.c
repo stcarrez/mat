00bba6815b458c27d2e5008190ca8e5cb0565de5
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-20 14:38:19 +0000

    Update copyright

--- libgpmat/src/gp-events.c
+++ libgpmat/src/gp-events.c
@@ -1,27 +1,21 @@
 /* gp-events.c -- Event operations
-   Copyright 2001 Free Software Foundation, Inc.
-   Written by Stephane Carrez (stcarrez@worldnet.fr)
-
-This file is part of gprofiler.
-
-gprofiler is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-gprofiler is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with EBCS; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
-
-#include "config.h"
+--  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+*/
+#include "gp-config.h"
 #include <stdarg.h>
-// #include "gprofiler.h"
 #include "gp-probe.h"
 #include "gp-events.h"
 
