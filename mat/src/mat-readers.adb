8c9af1998762e62f47f14e6cd068a940cc18280a
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-20 21:59:40 +0000

    - Implement the Read_Definition procedure - Use it in Read_Headers

--- mat/src/mat-readers.adb
+++ mat/src/mat-readers.adb
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Util.Log.Loggers;
 
 with MAT.Events;
 with MAT.Types;
@@ -23,65 +23,13 @@ with MAT.Readers.Marshaller;
 with Interfaces;
 package body MAT.Readers is
 
-   function Hash (Key : in MAT.Events.Internal_Reference) return Ada.Containers.Hash_Type is
+   --  The logger
+   Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("MAT.Readers");
+
+   function Hash (Key : in MAT.Types.Uint16) return Ada.Containers.Hash_Type is
    begin
       return Ada.Containers.Hash_Type (Key);
    end Hash;
-
---
---     function "<" (Left, Right : String_Ptr) return Boolean;
---     package Event_Def_Containers is
---       new BC.Containers (Item => Event_Description_Ptr);
---
---     package Event_Def_Trees is new Event_Def_Containers.Trees;
---
---     package Event_Def_AVL is
---       new Event_Def_Trees.AVL (Key => String_Ptr,
---                                Storage => Global_Heap.Storage);
-   --  Logical description of events received by the analysis tool.
-
-   type IpcManager_Base is new Manager_Base with record
-      Handlers    : Handler_Maps.Map;
---        Event_Types : Event_Def_AVL.AVL_Tree;
-   end record;
-   type IpcManager is access all IpcManager_Base'Class;
-
-   type ClientInfo is new MAT.Clients.ClientInfo with record
-      Ipc_Channel : Object_Ptr;
-      Adapter     : aliased IpcManager_Base;
-   end record;
-   type ClientInfo_Ptr is access all ClientInfo;
---
---     function "<" (Left, Right : String_Ptr) return Boolean is
---     begin
---        return Left.all < Right.all;
---     end "<";
-
-   --  Return the object adapter manager which holds all the servant
-   --  for the client.
---
---     procedure Create_Instance (Refs : in ClientInfo_Ref_Map) is
---        Adapter : Manager       := Get_Manager (Refs);
---        --Client  : Client_Memory := new Client_Memory;
---     begin
---        -- Insert (Refs, "memory", Client);
---        -- Register_Servant (Adapter, Proxy);
---        null;
---     end Create_Instance;
-
-
---     function Get_Manager (Refs : in ClientInfo_Ref_Map) return Manager is
---        Info : MAT.Clients.ClientInfo_Ref := Find (Refs, "com-channel");
---     begin
---        if Info.all not in ClientInfo'Class then
---           raise PROGRAM_ERROR;
---        end if;
---        declare
---           Channel : ClientInfo_Ptr := ClientInfo (Info.all)'Access;
---        begin
---           return Channel.Adapter'Access;
---        end;
---     end Get_Manager;
 
    procedure Register_Servant (Adapter : in Manager;
                                Proxy : in Servant) is
@@ -91,6 +39,19 @@ package body MAT.Readers is
       end if;
       Proxy.Owner := Adapter;
    end Register_Servant;
+
+   procedure Register_Reader (Into  : in out Manager_Base;
+                              Name  : in String;
+                              Id    : in MAT.Events.Internal_Reference;
+                              Model : in MAT.Events.Attribute_Table_Ptr) is
+      Handler : Message_Handler;
+   begin
+      Handler.Id := Id;
+      Handler.Attributes  := Model;
+      Handler.Mapping     := new MAT.Events.Attribute_Table (Model'Range);
+      Handler.Mapping.all := Model.all;
+      Into.Readers.Insert (Name, Handler);
+   end Register_Reader;
 
    procedure Register_Message_Analyzer (Proxy  : in Reader_Access;
                                         Name   : in String;
@@ -141,37 +102,59 @@ package body MAT.Readers is
          declare
             Handler : constant Message_Handler := Handler_Maps.Element (Pos);
          begin
-            Dispatch (Handler.For_Servant.all, Handler.Id, Handler.Mapping, Msg);
+            Dispatch (Handler.For_Servant.all, Handler.Id, Handler.Mapping.all'Access, Msg);
          end;
       end if;
    end Dispatch_Message;
 
+   --  ------------------------------
+   --  Read an event definition from the stream and configure the reader.
+   --  ------------------------------
+   procedure Read_Definition (Client : in out Manager_Base;
+                              Msg    : in out Message) is
+      Name  : constant String := MAT.Readers.Marshaller.Get_String (Msg.Buffer);
+      Event : constant MAT.Types.Uint16 := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
+      Count : constant MAT.Types.Uint8 := MAT.Readers.Marshaller.Get_Uint8 (Msg.Buffer);
+      Pos   : constant Reader_Maps.Cursor := Client.Readers.Find (Name);
+
+      procedure Read_Attributes (Key     : in String;
+                                 Element : in out Message_Handler) is
+      begin
+         Element.Mapping := new MAT.Events.Attribute_Table (1 .. Natural (Count));
+         for I in 1 .. Natural (Count) loop
+            declare
+               Name : constant String := MAT.Readers.Marshaller.Get_String (Msg.Buffer);
+               Size : constant MAT.Types.Uint16 := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
+            begin
+               for J in Element.Attributes'Range loop
+                  if Element.Attributes (J).Name.all = Name then
+                     Element.Mapping (I) := Element.Attributes (J);
+                  end if;
+               end loop;
+            end;
+         end loop;
+      end Read_Attributes;
+
+   begin
+      Log.Debug ("Read event definition {0}", Name);
+      if Reader_Maps.Has_Element (Pos) then
+         Client.Readers.Update_Element (Pos, Read_Attributes'Access);
+      end if;
+   end Read_Definition;
+
    procedure Read_Headers (Client : in out Manager_Base;
                            Msg    : in out Message) is
       Count : MAT.Types.Uint16;
-
    begin
       Client.Version := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
       Client.Flags   := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
       Count := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
+
+      Log.Info ("Read event stream version {0} with {1} definitions",
+                MAT.Types.Uint16'Image (Client.Version),
+                MAT.Types.Uint16'Image (Count));
       for I in 1 .. Count loop
-         declare
-            Name  : constant String := MAT.Readers.Marshaller.Get_String (Msg.Buffer);
-            Event : constant MAT.Types.Uint16 := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
-            Cnt   : constant MAT.Types.Uint8 := MAT.Readers.Marshaller.Get_Uint8 (Msg.Buffer);
-            Pos   : constant Cursor := Client.Readers.Find (Name);
-
-            procedure Read_Attribute_Definition (Element : in out Event_Description) is
-               Name : constant String := MAT.Readers.Marshaller.Get_String (Msg.Buffer);
-               Size : constant MAT.Types.Uint16 := MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer);
-            begin
-            end Read_Attribute_Definition;
-
-         begin
-            if Has_Element (Pos) then
-               Client.Readers.Update_Element (Pos, Read_Attribute_Definition'Access);
-            end if;
-         end;
+         Read_Definition (Client, Msg);
       end loop;
    end Read_Headers;
 
