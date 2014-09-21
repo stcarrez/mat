260bef1459791d02c8aaf2c21ebae4f6d45dab71
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-09-21 13:26:32 +0000

    Add unit tests to read an event data stream file

---
+++ mat/regtests/mat-readers-tests.ads
@@ -0,0 +1,29 @@
+-----------------------------------------------------------------------
+--  mat-readers-tests -- Unit tests for MAT readers
+--  Copyright (C) 2014 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+
+package MAT.Readers.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   procedure Test_Read_File (T : in out Test);
+
+end MAT.Readers.Tests;
