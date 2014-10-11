35208ed156fcb24f3e3a0fed13c2f6da66e3e1a6
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-10-11 15:01:26 +0000

    Implement the protected operation Size_Information

--- mat/src/memory/mat-memory-targets.adb
+++ mat/src/memory/mat-memory-targets.adb
@@ -34,7 +34,6 @@ package body MAT.Memory.Targets is
    begin
       Memory.Reader := Memory_Reader.all'Access;
       Memory_Reader.Data := Memory'Unrestricted_Access;
-      Memory.Frames := Frames.Create_Root;
       MAT.Memory.Readers.Register (Reader, Memory_Reader);
    end Initialize;
 
@@ -232,6 +231,41 @@ package body MAT.Memory.Targets is
       begin
          MAT.Frames.Insert (Frames, Pc, Result);
       end Create_Frame;
+
+      --  ------------------------------
+      --  Collect the information about memory slot sizes allocated by the application.
+      --  ------------------------------
+      procedure Size_Information (Sizes  : in out Size_Info_Map) is
+         Iter : Allocation_Cursor := Used_Slots.First;
+
+         procedure Update_Count (Size : in MAT.Types.Target_Size;
+                                 Info : in out Size_Info_Type) is
+         begin
+            Info.Count := Info.Count + 1;
+         end Update_Count;
+
+         procedure Collect (Addr : in MAT.Types.Target_Addr;
+                            Slot : in Allocation) is
+            Pos : Size_Info_Cursor := Sizes.Find (Slot.Size);
+         begin
+            if Size_Info_Maps.Has_Element (Pos) then
+               Sizes.Update_Element (Pos, Update_Count'Access);
+            else
+               declare
+                  Info : Size_Info_Type;
+               begin
+                  Info.Count := 1;
+                  Sizes.Insert (Slot.Size, Info);
+               end;
+            end if;
+         end Collect;
+
+      begin
+         while Allocation_Maps.Has_Element (Iter) loop
+            Allocation_Maps.Query_Element (Iter, Collect'Access);
+            Allocation_Maps.Next (Iter);
+         end loop;
+      end Size_Information;
 
    end Memory_Allocator;
 
