fd1feacc3add07d0648ee46a5bd35ae715cd2b54
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-10-12 09:07:42 +0000

    Fix compilation warnings and use MAT.Types.Hex_Image

--- mat/src/mat-events.adb
+++ mat/src/mat-events.adb
@@ -15,74 +15,56 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Interfaces;
-with Ada.Text_IO; use Ada.Text_IO;
-with MAT.Types;
+
+with Ada.Text_IO;
 package body MAT.Events is
 
    procedure Dump (Value : in Event_Data; Attr : in Attribute);
-   function Hex_Image (Val : MAT.Types.Uint32; Len : Positive) return String;
-
-   function Hex_Image (Val : in MAT.Types.Uint32; Len : Positive) return String is
-      Conversion : constant String (1 .. 16) := "0123456789ABCDEF";
-      S : String (1 .. Len) := (others => '0');
-      P : MAT.Types.Uint32 := Val;
-      N : MAT.Types.Uint32;
-      I : Positive := Len;
-   begin
-      while P /= 0 loop
-         N := P mod 16;
-         P := P / 16;
-         S (I) := Conversion (Natural (N + 1));
-         exit when I = 1;
-         I := I - 1;
-      end loop;
-      return S;
-   end Hex_Image;
 
    procedure Dump (Value : in Event_Data; Attr : in Attribute) is
+      use type Ada.Text_IO.Positive_Count;
    begin
-      Put ("  " & Attr.Name.all);
-      Set_Col (30);
+      Ada.Text_IO.Put ("  " & Attr.Name.all);
+      Ada.Text_IO.Set_Col (30);
       case Value.Kind is
          when T_UINT8 =>
-            Put (MAT.Types.Uint8'Image (Value.U8));
+            Ada.Text_IO.Put (MAT.Types.Uint8'Image (Value.U8));
 
          when T_UINT16 =>
-            Put (MAT.Types.Uint16'Image (Value.U16));
+            Ada.Text_IO.Put (MAT.Types.Uint16'Image (Value.U16));
 
          when T_UINT32 | T_SIZE_T =>
-            Put (MAT.Types.Uint32'Image (Value.U32));
+            Ada.Text_IO.Put (MAT.Types.Uint32'Image (Value.U32));
 
          when T_POINTER =>
-            Put (Hex_Image (Value.U32, 8));
+            Ada.Text_IO.Put (MAT.Types.Hex_Image (Value.U32, 8));
 
          when T_UINT64 =>
-            Put (MAT.Types.Uint64'Image (Value.U64));
+            Ada.Text_IO.Put (MAT.Types.Uint64'Image (Value.U64));
 
          when T_PROBE =>
-            Put_Line ("Th " & MAT.Types.Uint32'Image (Value.Probe.Thread));
-            Set_Col (29);
-            for I in 1 .. Natural (Value.Probe.Cur_Depth) loop
+            Ada.Text_IO.Put_Line ("Th " & MAT.Types.Uint32'Image (Value.Probe.Thread));
+            Ada.Text_IO.Set_Col (29);
+            for I in 1 .. Value.Probe.Cur_Depth loop
                exit when I > Value.Probe.Frame'Last;
-               if Col > 80 - 14 then
-                  Set_Col (29);
+               if Ada.Text_IO.Col > 80 - 14 then
+                  Ada.Text_IO.Set_Col (29);
                end if;
-               Put (" {"
-                    & Hex_Image (Unsigned_32 (Value.Probe.Frame (I)), 8)
-                    & "}");
+               Ada.Text_IO.Put (" {"
+                                & MAT.Types.Hex_Image (Unsigned_32 (Value.Probe.Frame (I)), 8)
+                                & "}");
             end loop;
 
          when others =>
             null;
       end case;
-      New_Line;
+      Ada.Text_IO.New_Line;
    end Dump;
 
    procedure Dump (Table : in Event_Data_Table;
                    Def   : in Event_Description) is
    begin
-      Put_Line (Ada.Strings.Unbounded.To_String (Def.Name) & ":");
+      Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (Def.Name) & ":");
       for I in 1 .. Def.Nb_Attributes loop
          Dump (Table (I), Def.Def (I));
       end loop;
