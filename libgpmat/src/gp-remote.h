b2288c771bca1c3297f9712bee750fa466da80f6
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-10-22 19:41:44 +0000

    Define gp_buffered_server type and gp_buffered_server_initialize operation

--- libgpmat/src/gp-remote.h
+++ libgpmat/src/gp-remote.h
@@ -1,5 +1,5 @@
 /* Remote access
---  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2014 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,7 +19,10 @@
 
 #define GP_VERSION 1
 
+#define DEFAULT_TCP_PORT 4096
+
 struct gp_server;
+struct gp_buffered_server;
 
 typedef void (* gp_remote_send_t) (struct gp_server *, const void *, size_t);
 
@@ -27,11 +30,22 @@ typedef int (* gp_remote_sync_t) (struct gp_server *);
 
 typedef void (* gp_remote_close_t) (struct gp_server *);
 
+typedef int (* gp_remote_flush_t) (struct gp_buffered_server *);
+
 struct gp_server
 {
-  gp_remote_send_t to_send;
-  gp_remote_sync_t to_synchronize;
+  gp_remote_send_t  to_send;
+  gp_remote_sync_t  to_synchronize;
   gp_remote_close_t to_close;
+};
+
+struct gp_buffered_server
+{
+  struct gp_server  root;
+  gp_remote_flush_t to_flush;
+  unsigned char*    write_ptr;
+  unsigned char*    last_ptr;
+  unsigned char     buffer[4096];
 };
 
 extern void gp_dump (const char* title, int indent, const void* addr, size_t len);
@@ -45,5 +59,12 @@ extern void gp_remote_sync (void);
 extern void gp_remote_close (void);
 
 extern int gp_remote_initialize (void);
+
+/**
+ * @brief Initialize the buffered server instance.
+ *
+ * @param server the server instance.
+ */
+extern void gp_buffered_server_initialize (struct gp_buffered_server* server);
 
 #endif
