e8a52292681212ba6fa2cdb28551bd3430dcbe46
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-11-02 17:24:05 +0000

    Define the Socket_Listener type to create the accept socket and wait for connections

--- mat/src/mat-readers-streams-sockets.ads
+++ mat/src/mat-readers-streams-sockets.ads
@@ -15,35 +15,45 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Finalization;
+
 with Util.Streams.Buffered;
 with Util.Streams.Files;
 with Util.Streams.Sockets;
 with GNAT.Sockets;
 package MAT.Readers.Streams.Sockets is
 
-   type Socket_Reader_Type is new Manager_Base with private;
+   type Socket_Listener is new Ada.Finalization.Limited_Controlled with private;
+
+   type Socket_Reader_Type is new MAT.Readers.Streams.Stream_Reader_Type with private;
    type Socket_Reader_Type_Access is access all Socket_Reader_Type'Class;
 
    --  Open the socket to accept connections.
    procedure Open (Reader  : in out Socket_Reader_Type;
                    Address : in GNAT.Sockets.Sock_Addr_Type);
 
-   procedure Read_All (Reader : in out Socket_Reader_Type);
-
    procedure Close (Reader : in out Socket_Reader_Type);
 
 private
+
+   task type Socket_Listener_Task is
+      entry Start (Address : in GNAT.Sockets.Sock_Addr_Type);
+   end Socket_Listener_Task;
 
    task type Socket_Reader_Task is
       entry Start (S : in Socket_Reader_Type_Access;
                    Address : in GNAT.Sockets.Sock_Addr_Type);
    end Socket_Reader_Task;
 
-   type Socket_Reader_Type is new Manager_Base with record
+   type Socket_Reader_Type is new MAT.Readers.Streams.Stream_Reader_Type with record
       Socket : aliased Util.Streams.Sockets.Socket_Stream;
-      Stream : Util.Streams.Buffered.Buffered_Stream;
       Server : Socket_Reader_Task;
       Stop   : Boolean := False;
+   end record;
+
+   type Socket_Listener is new Ada.Finalization.Limited_Controlled with record
+      Accept_Selector : GNAT.Sockets.Selector_Type;
+      Listener        : Socket_Listener_Task;
    end record;
 
 end MAT.Readers.Streams.Sockets;
