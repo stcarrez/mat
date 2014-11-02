8221784a09504bacc56571e675eac216134933be
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-11-02 15:36:45 +0000

    Remove the Read_All procedure which is now defined by MAT.Readers.Streams package

--- mat/src/mat-readers-files.adb
+++ mat/src/mat-readers-files.adb
@@ -24,9 +24,6 @@ package body MAT.Readers.Files is
    --  The logger
    Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("MAT.Readers.Files");
 
-   BUFFER_SIZE  : constant Natural := 100 * 1024;
-   MAX_MSG_SIZE : constant Ada.Streams.Stream_Element_Offset := 2048;
-
    --  Open the file.
    procedure Open (Reader : in out File_Reader_Type;
                    Path   : in String) is
@@ -39,59 +36,5 @@ package body MAT.Readers.Files is
       Reader.File.Open (Mode => Ada.Streams.Stream_IO.In_File,
                         Name => Path);
    end Open;
-
-   procedure Read_All (Reader : in out File_Reader_Type) is
-      use Ada.Streams;
-      use type MAT.Types.Uint8;
-
-      Data    : aliased Ada.Streams.Stream_Element_Array (0 .. MAX_MSG_SIZE);
-      Buffer  : aliased Buffer_Type;
-      Msg     : Message;
-      Last    : Ada.Streams.Stream_Element_Offset;
-      Format  : MAT.Types.Uint8;
-   begin
-      Msg.Buffer := Buffer'Unchecked_Access;
-      Msg.Buffer.Start := Data (0)'Address;
-      Msg.Buffer.Current := Msg.Buffer.Start;
-      Msg.Buffer.Last := Data (MAX_MSG_SIZE)'Address;
-      Msg.Buffer.Size := 3;
-      Reader.Stream.Read (Data (0 .. 2), Last);
-      Format := MAT.Readers.Marshaller.Get_Uint8 (Msg.Buffer);
-      if Format = 0 then
-         Msg.Buffer.Endian := LITTLE_ENDIAN;
-         Log.Debug ("Data stream is little endian");
-      else
-         Msg.Buffer.Endian := BIG_ENDIAN;
-         Log.Debug ("Data stream is big endian");
-      end if;
-      Msg.Size := Natural (MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer));
-      if Msg.Size < 2 then
-         Log.Error ("Invalid message size {0}", Natural'Image (Msg.Size));
-      end if;
-      Reader.Stream.Read (Data (0 .. Ada.Streams.Stream_Element_Offset (Msg.Size - 1)), Last);
-      Msg.Buffer.Current := Msg.Buffer.Start;
-      Msg.Buffer.Last    := Data (Last)'Address;
-      Msg.Buffer.Size    := Msg.Size;
-      Reader.Read_Headers (Msg);
-      while not Reader.Stream.Is_Eof loop
-         Reader.Stream.Read (Data (0 .. 1), Last);
-         exit when Last /= 2;
-         Msg.Buffer.Size := 2;
-         Msg.Buffer.Current := Msg.Buffer.Start;
-         Msg.Size := Natural (MAT.Readers.Marshaller.Get_Uint16 (Msg.Buffer));
-         if Msg.Size < 2 then
-            Log.Error ("Invalid message size {0}", Natural'Image (Msg.Size));
-         end if;
-         if Ada.Streams.Stream_Element_Offset (Msg.Size) >= Data'Last then
-            Log.Error ("Message size {0} is too big", Natural'Image (Msg.Size));
-            exit;
-         end if;
-         Reader.Stream.Read (Data (0 .. Ada.Streams.Stream_Element_Offset (Msg.Size - 1)), Last);
-         Msg.Buffer.Current := Msg.Buffer.Start;
-         Msg.Buffer.Last    := Data (Last)'Address;
-         Msg.Buffer.Size    := Msg.Size;
-         Reader.Dispatch_Message (Msg);
-      end loop;
-   end Read_All;
 
 end MAT.Readers.Files;
