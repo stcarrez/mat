552042f46979b098fdea3a1ae587f1b12d9581c0
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-11-09 22:44:01 +0000

    Fix compilation warnings

--- mat/src/mat-readers-streams-files.ads
+++ mat/src/mat-readers-streams-files.ads
@@ -15,7 +15,6 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Util.Streams.Buffered;
 with Util.Streams.Files;
 with MAT.Readers.Streams;
 package MAT.Readers.Streams.Files is
