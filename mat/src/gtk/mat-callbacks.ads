298efaf5a80af1eca0becc70ef6f1619791b1fc5
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-11-11 20:53:41 +0000

    Declare the On_Btn_Sizes procedure

--- mat/src/gtk/mat-callbacks.ads
+++ mat/src/gtk/mat-callbacks.ads
@@ -33,4 +33,7 @@ package MAT.Callbacks is
    --  Callback executed when the "close-about" action is executed from the about box.
    procedure On_Close_About (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
 
+   --  Callback executed when the "cmd-sizes" action is executed from the "Sizes" action.
+   procedure On_Btn_Sizes (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
+
 end MAT.Callbacks;
