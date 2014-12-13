7d354d0f33752443872f71c848b60758cad2746b
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-12-13 14:14:27 +0000

    - Update to use the MAT.Events.Probes new package - Define the Extract and Execute procedure

--- mat/src/mat-targets-probes.ads
+++ mat/src/mat-targets-probes.ads
@@ -16,40 +16,44 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 with MAT.Events;
+with MAT.Events.Targets;
 with MAT.Events.Probes;
 package MAT.Targets.Probes is
 
-   type Process_Servant is new MAT.Events.Probes.Probe_Type with record
+   type Process_Probe_Type is new MAT.Events.Probes.Probe_Type with record
       Target  : Target_Type_Access;
-      Reader  : MAT.Readers.Manager;
+      Manager : MAT.Events.Probes.Probe_Manager_Type_Access;
       Process : Target_Process_Type_Access;
       Events  : MAT.Events.Targets.Target_Events_Access;
    end record;
-   type Process_Reader_Access is access all Process_Servant'Class;
+   type Process_Probe_Type_Access is access all Process_Probe_Type'Class;
 
    --  Create a new process after the begin event is received from the event stream.
-   procedure Create_Process (For_Servant : in out Process_Servant;
-                             Pid         : in MAT.Types.Target_Process_Ref;
-                             Path        : in Ada.Strings.Unbounded.Unbounded_String);
+   procedure Create_Process (Probe : in out Process_Probe_Type;
+                             Pid   : in MAT.Types.Target_Process_Ref;
+                             Path  : in Ada.Strings.Unbounded.Unbounded_String);
 
+   --  Extract the probe information from the message.
    overriding
-   procedure Dispatch (For_Servant : in out Process_Servant;
-                       Id          : in MAT.Events.Internal_Reference;
-                       Params      : in MAT.Events.Const_Attribute_Table_Access;
-                       Frame       : in MAT.Events.Frame_Info;
-                       Msg         : in out MAT.Readers.Message);
+   procedure Extract (Probe  : in Process_Probe_Type;
+                      Params : in MAT.Events.Const_Attribute_Table_Access;
+                      Msg    : in out MAT.Readers.Message_Type;
+                      Event  : in out MAT.Events.Targets.Probe_Event_Type);
+
+   procedure Execute (Probe : in Process_Probe_Type;
+                      Event : in MAT.Events.Targets.Probe_Event_Type);
 
    --  Register the reader to extract and analyze process events.
-   procedure Register (Into   : in out MAT.Readers.Manager_Base'Class;
-                       Reader : in Process_Reader_Access);
+   procedure Register (Into  : in out MAT.Events.Probes.Probe_Manager_Type'Class;
+                       Probe : in Process_Probe_Type_Access);
 
    --  Initialize the target object to prepare for reading process events.
-   procedure Initialize (Target : in out Target_Type;
-                         Reader : in out MAT.Readers.Manager_Base'Class);
+   procedure Initialize (Target  : in out Target_Type;
+                         Manager : in out MAT.Events.Probes.Probe_Manager_Type_Access);
 
 private
 
-   procedure Probe_Begin (For_Servant : in out Process_Servant;
+   procedure Probe_Begin (Probe : in out Process_Probe_Type;
                           Id          : in MAT.Events.Internal_Reference;
                           Defs        : in MAT.Events.Attribute_Table;
                           Frame       : in MAT.Events.Frame_Info;
