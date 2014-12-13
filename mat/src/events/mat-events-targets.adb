e4b8dc7034829f3588a57fd37d1ffb14220ee746
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-12-13 17:11:57 +0000

    - Update the Insert procedure to insert events in the current event   block, create a new event block when it is full - Update the Get_Events protected procedure to find the first   event block and get the events from it

--- mat/src/events/mat-events-targets.adb
+++ mat/src/events/mat-events-targets.adb
@@ -22,10 +22,9 @@ package body MAT.Events.Targets is
    --  Add the event in the list of events and increment the event counter.
    --  ------------------------------
    procedure Insert (Target : in out Target_Events;
-                     Event  : in MAT.Types.Uint16;
-                     Frame  : in MAT.Events.Frame_Info) is
+                     Event  : in Probe_Event_Type) is
    begin
-      Target.Events.Insert (Event, Frame);
+      Target.Events.Insert (Event);
       Util.Concurrent.Counters.Increment (Target.Event_Count);
    end Insert;
 
@@ -50,23 +49,37 @@ package body MAT.Events.Targets is
       --  ------------------------------
       --  Add the event in the list of events.
       --  ------------------------------
-      procedure Insert (Event  : in MAT.Types.Uint16;
-                        Frame  : in MAT.Events.Frame_Info) is
+      procedure Insert (Event : in Probe_Event_Type) is
          Info : Target_Event;
       begin
-         Info.Event  := Event;
-         Info.Time   := Frame.Time;
-         Info.Thread := Frame.Thread;
-         Events.Insert (Frame.Time, Info);
+         if Current = null then
+            Current := new Event_Block;
+            Current.Start := Event.Time;
+            Events.Insert (Event.Time, Current);
+         end if;
+         Current.Count := Current.Count + 1;
+         Current.Events (Current.Count) := Event;
+         Current.Finish := Event.Time;
+         if Current.Count = Current.Events'Last then
+            Current := null;
+         end if;
       end Insert;
 
       procedure Get_Events (Start  : in MAT.Types.Target_Time;
                             Finish : in MAT.Types.Target_Time;
                             Into   : in out Target_Event_Vector) is
-         Iter : Event_Cursor := Events.First;
+         Iter  : Event_Cursor := Events.Floor (Start);
+         Block : Event_Block_Access;
       begin
          while Event_Maps.Has_Element (Iter) loop
-            Into.Append (Event_Maps.Element (Iter));
+            Block := Event_Maps.Element (Iter);
+            exit when Block.Start > Finish;
+            for I in Block.Events'First .. Block.Count loop
+               exit when Block.Events (I).Time > Finish;
+               if Block.Events (I).Time >= Start then
+                  Into.Append (Block.Events (I));
+               end if;
+            end loop;
             Event_Maps.Next (Iter);
          end loop;
       end Get_Events;
