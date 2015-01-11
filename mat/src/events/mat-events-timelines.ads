88b8f495fc8cb7b424db381fa0c0b00a0b3a2945
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-01-11 20:26:01 +0000

    New package to define timelines and identify timeline sections in the collected events

---
+++ mat/src/events/mat-events-timelines.ads
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------
+--  mat-events-timelines - Timelines
+--  Copyright (C) 2015 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Containers.Vectors;
+
+with MAT.Events.Targets;
+package MAT.Events.Timelines is
+
+   --  Describe a section of the timeline.  The section has a starting and ending
+   --  event that marks the boundary of the section within the collected events.
+   --  The timeline section gives the duration and some statistics about memory
+   --  allocation made in the section.
+   type Timeline_Info is record
+      Start_Id       : MAT.Events.Targets.Event_Id_Type;
+      Start_Time     : MAT.Types.Target_Tick_Ref;
+      End_Id         : MAT.Events.Targets.Event_Id_Type;
+      End_Time       : MAT.Types.Target_Tick_Ref;
+      Duration       : MAT.Types.Target_Time;
+      Malloc_Count   : Natural;
+      Realloc_Count  : Natural;
+      Free_Count     : Natural;
+   end record;
+
+   package Timeline_Info_Vectors is
+     new Ada.Containers.Vectors (Positive, Timeline_Info);
+
+end MAT.Events.Timelines;
