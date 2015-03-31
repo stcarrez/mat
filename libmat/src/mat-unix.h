afea237bbd4c8bce73179e5a401b4eb6fcc255d6
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-03-31 21:51:01 +0200

    Rename file prefix

--- libmat/src/mat-unix.h
+++ libmat/src/mat-unix.h
@@ -1,4 +1,4 @@
-/*  gp-unix.h -- Unix specific information
+/*  mat-unix.h -- Unix specific information
 --  Copyright (C) 2011, 2012, 2013, 2014, 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
