bd4cdee4a293d31c16fb4cc9731d2e9d1c7b4f9e
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-04-05 23:00:49 +0200

    Implement the Find_Frames procedure to collect function addresses that perform directly or indirectly malloc/realloc/free calls

--- matp/src/events/mat-events-timelines.adb
+++ matp/src/events/mat-events-timelines.adb
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with MAT.Frames;
 package body MAT.Events.Timelines is
 
    use MAT.Events.Targets;
@@ -207,5 +207,66 @@ package body MAT.Events.Timelines is
    begin
       Target.Iterate (Process => Collect_Event'Access);
    end Find_Sizes;
+
+   --  ------------------------------
+   --  Find the function address from the call event frames for the events which is selected
+   --  by the given filter.  The function addresses are collected up to the given frame depth.
+   --  Update the <tt>Frames</tt> map to keep track of the first event and last event and
+   --  the number of events found for the corresponding frame address.
+   --  ------------------------------
+   procedure Find_Frames (Target : in out MAT.Events.Targets.Target_Events'Class;
+                          Filter : in MAT.Expressions.Expression_Type;
+                          Depth  : in Natural;
+                          Frames : in out MAT.Events.Targets.Frame_Event_Info_Map) is
+      procedure Collect_Event (Event : in MAT.Events.Targets.Target_Event);
+
+      procedure Collect_Event (Event : in MAT.Events.Targets.Target_Event) is
+         procedure Update_Size (Size : in MAT.Types.Target_Size;
+                                Info : in out MAT.Events.Targets.Event_Info_Type);
+
+         procedure Update_Size (Size : in MAT.Types.Target_Size;
+                                Info : in out MAT.Events.Targets.Event_Info_Type) is
+            pragma Unreferenced (Size);
+         begin
+            Info.Count      := Info.Count + 1;
+            Info.Last_Event := Event;
+         end Update_Size;
+
+      begin
+         --  Look for events which are selected by the filter.
+         if not Filter.Is_Selected (Event) then
+            return;
+         end if;
+         declare
+            Backtrace : constant MAT.Frames.Frame_Table := MAT.Frames.Backtrace (Event.Frame);
+         begin
+            for I in Backtrace'Range loop
+               exit when I > Depth;
+               declare
+                  Pos : constant MAT.Events.Targets.Frame_Event_Info_Cursor
+                    := Frames.Find (Backtrace (I));
+               begin
+                  if MAT.Events.Targets.Frame_Event_Info_Maps.Has_Element (Pos) then
+                     --  Increment the count and update the last event.
+                     Frames.Update_Element (Pos, Update_Size'Access);
+                  else
+                     declare
+                        Info : Event_Info_Type;
+                     begin
+                        --  Insert a new size with the event.
+                        Info.First_Event := Event;
+                        Info.Last_Event := Event;
+                        Info.Count := 1;
+                        Frames.Insert (Backtrace (I), Info);
+                     end;
+                  end if;
+               end;
+            end loop;
+         end;
+      end Collect_Event;
+
+   begin
+      Target.Iterate (Process => Collect_Event'Access);
+   end Find_Frames;
 
 end MAT.Events.Timelines;
